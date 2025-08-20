238b7f309d54d4bfa84fa6b955b8d3a7345ee59b
TanbirAhamed <2203tanbir.ahamed@gmail.com>
2025-08-21 02:00:46 +0600

    Firebase added

---
+++ src/firebase/firebase.init.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCyUVE2MDmq9tKzPgiEPoDqr1DOoofnV4w",
+  authDomain: "travecta-545f7.firebaseapp.com",
+  projectId: "travecta-545f7",
+  storageBucket: "travecta-545f7.firebasestorage.app",
+  messagingSenderId: "276975968986",
+  appId: "1:276975968986:web:f8df93c022b7ecee6e7be3"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
