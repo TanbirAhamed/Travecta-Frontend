20cbd555309d29603b44475072539538ac31dbb3
Shahadat1530 <155170141+Shahadat1530@users.noreply.github.com>
2025-08-28 21:37:37 +0600

    created some auth functionality using firebase like- createUser, loginUser, logout, updateUser

--- src/provider/AuthProvider.jsx
+++ src/provider/AuthProvider.jsx
@@ -1,4 +1,7 @@
 import React, { createContext, useState } from 'react';
+import { createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword, signOut, updateProfile } from 'firebase/auth';
+import { auth } from '../firebase/firebase.init';
+import { useEffect } from 'react';
 
 
 export const AuthContext = createContext(null)
@@ -6,12 +9,47 @@ export const AuthContext = createContext(null)
 
 const AuthProvider = ({ children }) => {
     const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
 
+    const createNewUser = (email, password) => {
+        setLoading(true);
+        return createUserWithEmailAndPassword(auth, email, password);
+    };
+
+    const updateUserProfile = (updatedData) => {
+        return updateProfile(auth.currentUser, updatedData);
+    };
+
+    const userLogin = (email, password) => {
+        setLoading(true);
+        return signInWithEmailAndPassword(auth, email, password);
+    };
+
+    const logOut = () => {
+        setLoading(true);
+        return signOut(auth);
+    };
 
     const authInfo = {
-        user
-    }
+        user,
+        setUser,
+        loading,
+        setLoading,
+        createNewUser,
+        updateUserProfile,
+        userLogin,
+        logOut
+    };
 
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+            setUser(currentUser);
+            setLoading(false);
+        });
+        return () => {
+            unsubscribe();
+        };
+    }, []);
     return (
         <AuthContext.Provider value={authInfo}>
             {children}
