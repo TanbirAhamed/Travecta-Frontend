f5f2c8093123c0d84a787653dd7cd049deae7eee
TanbirAhamed <2203tanbir.ahamed@gmail.com>
2025-09-21 21:32:15 +0600

    some updated

--- src/pages/dashboard/adminDashboard/Home.jsx
+++ src/pages/dashboard/adminDashboard/Home.jsx
@@ -1,5 +1,6 @@
 import { Outlet } from 'react-router'
 import Tabs from './Tabs'
+import InfoSection from './InfoSection'
 
 const Home = () => {
   return (
@@ -7,6 +8,7 @@ const Home = () => {
         <div>
             <h1 className='text-4xl font-bold'>Admin Dashboard</h1>
             <p className='text-xl'>Manage trips, users, and platform content</p>
+            <InfoSection />
             <Tabs />
             <Outlet />
         </div>
